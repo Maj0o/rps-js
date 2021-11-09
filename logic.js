dfbfacb9c81208fb742f10e45a92063b525de72b
Quinten de Ruiter <qdderuiter@protonmail.ch>
2021-11-09 11:14:21 +0100

    added player input, round logic

--- logic.js
+++ logic.js
@@ -1,2 +1,22 @@
+function playerSelection() {
+  let playerSelection = prompt("Pick your choice! Rock, Paper or Scissors!");
+  return playerSelection.toLowerCase();
+}
 
+function computerPlay() {
+  let computerPlay = Math.floor(Math.random() * 3 + 1);
+  if (computerPlay === 1) {
+    computerPlay = "rock";
+  } else if (computerPlay === 2) {
+    computerPlay = "paper";
+  } else {
+    computerPlay = "scissors";
+  }
+  return computerPlay;
+}
 
+function playRound(playerSelection, computerSelection) {}
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerPlay));
