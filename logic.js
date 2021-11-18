7e1334659d659bf3762a2b335af7aaa18de60da2
Quinten de Ruiter <qdderuiter@protonmail.ch>
2021-11-18 17:00:06 +0100

    added rounds, fixed logic

--- logic.js
+++ logic.js
@@ -4,42 +4,65 @@ let amountOfRounds = 0;
 let winMessage = "You win!";
 let lossMessage = "You lose!";
 let tieMessage = "It's a tie!";
+const PAPER = "paper";
+const SCISSORS = "scissors";
+const ROCK = "rock";
+
+const computerPlay = () => {
+  let number = Math.floor(Math.random() * 3) + 1;
+  if (number === 1) {
+    return ROCK;
+  } else if (number === 2) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
 
 let playerSelection = () => {
   let playerSelection = prompt("Pick your choice! Rock, Paper or Scissors!");
   return playerSelection.toLowerCase();
 };
 
-const computerPlay = () => {
-  let number = Math.floor(Math.random() * 3) + 1;
-  if (number === 1) {
-    return "Rock";
-  } else if (number === 2) {
-    return "Paper";
-  } else {
-    return "Scissors";
-  }
-};
-
 const playRound = (playerSelection, computerPlay) => {
-  amountOfRounds++;
-  gameLogic();
-};
-
-const gameLogic = () => {
+  scoreBoard();
   //Rock options
-  if (playerSelection == "rock" && "scissors") {
+  amountOfRounds++;
+  if (playerSelection == ROCK && computerPlay == SCISSORS) {
+    playerScore++;
     return winMessage;
-  } else if (playerSelection == "rock" && "paper") {
+  } else if (playerSelection == ROCK && computerPlay == PAPER) {
+    computerScore++;
     return lossMessage;
-  } else if (playerSelection == "rock" && "rock") {
+  } else if (playerSelection == ROCK && computerPlay == ROCK) {
     return tieMessage;
-  }
+    //Paper options
+  } else if (playerSelection == PAPER && computerPlay == ROCK) {
+    playerScore++;
+    return lossMessage;
+  } else if (playerSelection == PAPER && computerPlay == SCISSORS) {
+    computerScore++;
+    return lossMessage;
+  } else if (playerSelection == PAPER && computerPlay == PAPER) {
+    return tieMessage;
+    //Scissor options
+  } else if (playerSelection == SCISSORS && computerPlay == ROCK) {
+    computerScore++;
+    return lossMessage;
+  } else if (playerSelection == SCISSORS && computerPlay == PAPER) {
+    playerScore++;
+    return winMessage;
+  } else playerSelection == SCISSORS && computerPlay == SCISSORS;
+  return tieMessage;
 };
 
 const scoreBoard = () => {
   const computerScoreDisplay = document.getElementById("computerScore");
+  computerScoreDisplay.innerText = "CPU: " + computerScore;
   const playerScoreDisplay = document.getElementById("playerScore");
-  computerScoreDisplay.innerText = computerScore;
-  playerScoreDisplay.innerText = playerScore;
+  playerScoreDisplay.innerText = "Player: " + playerScore;
+  const amountOfRoundsDisplay = document.getElementById("amountOfRounds");
+  amountOfRoundsDisplay.innerText = "Rounds: " + amountOfRounds;
 };
+
+console.log(playRound(playerSelection(), computerPlay()));
