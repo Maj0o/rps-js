dfab5c5d4f07839a0014246b5de457165891de8f
Quinten <qdderuiter@protonmail.com>
2021-12-04 17:47:16 +0100

    cleaned up OnClick event

--- logic.js
+++ logic.js
@@ -39,77 +39,67 @@ let playerSelection = () => {
 };
 
 function playRock(playerSelection, computerPlay) {
-  document.getElementById("image-rock").addEventListener(
-    "click",
-    () => {
-      document.getElementById("displayresult").innerHTML = "Hello World";
-
-      if (playerSelection == CHOICES.ROCK && computerPlay == CHOICES.SCISSORS) {
-        playerScore++;
-        amountOfRounds++;
-        return `${MESSAGES.winMessage} ${CHOICES.ROCK} crushes ${CHOICES.SCISSORS}`;
-      } else if (
-        playerSelection == CHOICES.ROCK &&
-        computerPlay == CHOICES.PAPER
-      ) {
-        computerScore++;
-        amountOfRounds++;
-        return `${MESSAGES.lossMessage} ${CHOICES.PAPER} covers ${CHOICES.ROCK}`;
-      } else if (
-        playerSelection == CHOICES.ROCK &&
-        computerPlay == CHOICES.ROCK
-      ) {
-        amountOfRounds++;
-        return `${MESSAGES.tieMessage} ${CHOICES.ROCK} can't crush ${CHOICES.ROCK}`;
-      }
-    },
-
-    function playPaper(playerSelection, computerPlay) {
-      if (playerSelection == CHOICES.PAPER && computerPlay == CHOICES.ROCK) {
-        playerScore++;
-        amountOfRounds++;
-        return `${MESSAGES.winMessage} ${CHOICES.PAPER} covers ${CHOICES.ROCK}`;
-      } else if (
-        playerSelection == CHOICES.PAPER &&
-        computerPlay == CHOICES.SCISSORS
-      ) {
-        computerScore++;
-        amountOfRounds++;
-        return `${MESSAGES.lossMessage} ${CHOICES.PAPER} gets cut by ${CHOICES.SCISSORS}`;
-      } else if (
-        playerSelection == CHOICES.PAPER &&
-        computerPlay == CHOICES.PAPER
-      ) {
-        amountOfRounds++;
-        return `${MESSAGES.tieMessage} ${CHOICES.PAPER} can't cover ${CHOICES.PAPER}`;
-      }
-    }
-  );
-
-  function playScissors(playerSelection, computerPlay) {
-    if (playerSelection == CHOICES.SCISSORS && computerPlay == CHOICES.ROCK) {
-      computerScore++;
-      amountOfRounds++;
-      return `${MESSAGES.lossMessage} ${CHOICES.SCISSORS} gets crushed by ${CHOICES.ROCK}`;
-    } else if (
-      playerSelection == CHOICES.SCISSORS &&
-      computerPlay == CHOICES.PAPER
-    ) {
-      playerScore++;
-      amountOfRounds++;
-      return `${MESSAGES.winMessage} ${CHOICES.SCISSORS} cuts ${CHOICES.PAPER}`;
-    } else
-      playerSelection == CHOICES.SCISSORS && computerPlay == CHOICES.SCISSORS;
+  if (playerSelection == CHOICES.ROCK && computerPlay == CHOICES.SCISSORS) {
+    playerScore++;
     amountOfRounds++;
-    return `${MESSAGES.tieMessage} ${CHOICES.SCISSORS} can't cut ${CHOICES.SCISSORS}`;
+    return `${MESSAGES.winMessage} ${CHOICES.ROCK} crushes ${CHOICES.SCISSORS}`;
+  } else if (playerSelection == CHOICES.ROCK && computerPlay == CHOICES.PAPER) {
+    computerScore++;
+    amountOfRounds++;
+    return `${MESSAGES.lossMessage} ${CHOICES.PAPER} covers ${CHOICES.ROCK}`;
+  } else if (playerSelection == CHOICES.ROCK && computerPlay == CHOICES.ROCK) {
+    amountOfRounds++;
+    return `${MESSAGES.tieMessage} ${CHOICES.ROCK} can't crush ${CHOICES.ROCK}`;
   }
-  // function scoreBoard() {
-  //   const computerScoreDisplay = document.getElementById("computerscore");
-  //   const playerScoreDisplay = document.getElementById("playerscore");
-  //   computerScoreDisplay.innerText = `Computer: ${computerScore}`;
-  //   playerScoreDisplay.innerText = `Player: ${playerScore}`;
-  //
 }
+
+function playPaper(playerSelection, computerPlay) {
+  if (playerSelection == CHOICES.PAPER && computerPlay == CHOICES.ROCK) {
+    playerScore++;
+    amountOfRounds++;
+    return `${MESSAGES.winMessage} ${CHOICES.PAPER} covers ${CHOICES.ROCK}`;
+  } else if (
+    playerSelection == CHOICES.PAPER &&
+    computerPlay == CHOICES.SCISSORS
+  ) {
+    computerScore++;
+    amountOfRounds++;
+    return `${MESSAGES.lossMessage} ${CHOICES.PAPER} gets cut by ${CHOICES.SCISSORS}`;
+  } else if (
+    playerSelection == CHOICES.PAPER &&
+    computerPlay == CHOICES.PAPER
+  ) {
+    amountOfRounds++;
+    return `${MESSAGES.tieMessage} ${CHOICES.PAPER} can't cover ${CHOICES.PAPER}`;
+  }
+}
+
+function playScissors(playerSelection, computerPlay) {
+  if (playerSelection == CHOICES.SCISSORS && computerPlay == CHOICES.ROCK) {
+    computerScore++;
+    amountOfRounds++;
+    return `${MESSAGES.lossMessage} ${CHOICES.SCISSORS} gets crushed by ${CHOICES.ROCK}`;
+  } else if (
+    playerSelection == CHOICES.SCISSORS &&
+    computerPlay == CHOICES.PAPER
+  ) {
+    playerScore++;
+    amountOfRounds++;
+    return `${MESSAGES.winMessage} ${CHOICES.SCISSORS} cuts ${CHOICES.PAPER}`;
+  } else
+    playerSelection == CHOICES.SCISSORS && computerPlay == CHOICES.SCISSORS;
+  amountOfRounds++;
+  return `${MESSAGES.tieMessage} ${CHOICES.SCISSORS} can't cut ${CHOICES.SCISSORS}`;
+}
+
+console.log(playScissors(playerSelection(), computerPlay()));
+
+// function scoreBoard() {
+//   const computerScoreDisplay = document.getElementById("computerscore");
+//   const playerScoreDisplay = document.getElementById("playerscore");
+//   computerScoreDisplay.innerText = `Computer: ${computerScore}`;
+//   playerScoreDisplay.innerText = `Player: ${playerScore}`;
+//
 
 // function scoreBoard() {
 //   const computerScoreDisplay = document.getElementById("computerscore");
